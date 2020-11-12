6428dc1bae60aad45296d50d5936a4da83c73b3f
fabiomedda <meddafabio@gmail.com>
2020-11-12 18:16:02 +0100

    pass

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+var nome = prompt("Qual'è il tuo nome?");
+var cognome = prompt("Qual'è il tuo cognome?");
+var colorePreferito = prompt("Qual'è il tuo colore preferito?");
+document.getElementById("pass").innerHTML = nome + cognome + colorePreferito + "19";
